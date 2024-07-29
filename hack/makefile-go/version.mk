1d3fce24249608e15aa70082739d9f027f74b3b0
alswl <alswlx@gmail.com>
2024-07-29 12:08:09 +0000

    fix: bump new version

--- hack/makefile-go/version.mk
+++ hack/makefile-go/version.mk
@@ -3,5 +3,5 @@ STAGE=$(DEFAULT_BUMP_STAGE)
 SCOPE=$(DEFAULT_BUMP_SCOPE)
 DRY_RUN=$(DEFAULT_BUMP_DRY_RUN)
 bump: check-git-status ## Bump version
-	(bash ./hack/bump.sh ${STAGE} ${SCOPE} ${DRY_RUN})
+	(bash ./hack/bump.sh --stage ${STAGE} --scope ${SCOPE} --dry-run ${DRY_RUN})
 
